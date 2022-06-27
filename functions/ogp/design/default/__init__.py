f9dddf1e146ed1e3ed1ba2846efad88f764a4f16
ShotaroKataoka <s183347@stn.nagaokaut.ac.jp>
2022-06-27 21:02:26 +0900

    🐛 ogp defautl

--- functions/ogp/design/default/__init__.py
+++ functions/ogp/design/default/__init__.py
@@ -66,7 +66,9 @@ class Design(AbstractDesign):
         font = ImageFont.truetype(self.font_black_path, self.title_font_size)
         text = self.title_texts[0]
         multiline = False
-        for i, title_text in enumerate(self.title_texts[1:]):
+        for i, title_text in enumerate(self.title_texts):
+            if i==0:
+                continue
             if is_text_size_ok(draw, font, text+title_text, base_img.size[0], self.side_padding, text_padding=250):
                 text += title_text
             else:
